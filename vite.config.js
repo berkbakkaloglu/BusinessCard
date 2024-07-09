8c36b1417dfcfb6ba3e8b58e8eacb957c05c7622
berkbakkaloglu <148460203+berkbakkaloglu@users.noreply.github.com>
2024-07-09 19:22:45 +0300

    New try

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-    build: {
-      rollupOptions: {
-        input: 'index.jsx',
-      },
-    },
- 
 })
 
